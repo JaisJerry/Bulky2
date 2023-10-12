b69c8e2a2bc658a1fb8ae75df2bceecee8da8013
Jaison <129634572+JaisJerry@users.noreply.github.com>
2023-10-12 18:59:16 +0100

    Identity

--- Bulky2.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky2.DataAccess/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using Bulky2.Models;
 using Bulky2.Models.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Bulky2.Data
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
--- Bulky2Web/Program.cs
+++ Bulky2Web/Program.cs
@@ -2,12 +2,16 @@ using Bulky2.Data;
 using Bulky2.DataAccess.Repository;
 using Bulky2.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -24,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
